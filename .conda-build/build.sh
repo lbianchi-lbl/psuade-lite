62846451088fc9ed2e7557db296c22e96603ef94
Ludovico Bianchi <lbianchi@lbl.gov>
2021-03-09 19:36:28 -0800

    Try passing CONDA_BUILD_SYSROOT to CMake

--- .conda-build/build.sh
+++ .conda-build/build.sh
@@ -7,6 +7,6 @@ BIN_PATH="$PREFIX/bin"
 
 mkdir -pv "$BIN_PATH"
 
-cmake "$SOURCE_PATH" -DCMAKE_CXX_FLAGS="--std=c++14" -DCMAKE_INSTALL_PREFIX="$PREFIX"
+cmake "$SOURCE_PATH" -DCMAKE_CXX_FLAGS="--std=c++14" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_OSX_SYSROOT="$CONDA_BUILD_SYSROOT"
 cmake --build . --target install
 # cp ./bin/* "$BIN_PATH"
